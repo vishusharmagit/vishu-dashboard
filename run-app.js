53635841617a30886ea907f480a597381409e1de
Vishu <vishu@getperx.com>
2016-12-16 16:18:37 +0800

    Added theme dependency.

--- run-app.js
+++ run-app.js
@@ -2,5 +2,6 @@ require('shelljs/global');
 
 exec('npm install');
 exec('bower install');
+exec('ember install:addon node_modules/vishu-dashboard-theme');
 exec('ember install:addon node_modules/vishu-components');
 
