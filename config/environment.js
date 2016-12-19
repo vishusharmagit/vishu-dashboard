ae21df66c031c5c3c7172ea92c41ddf9cfc9ad93
Vishu <vishu@getperx.com>
2016-12-19 14:44:29 +0800

    Added moment dependency.

--- config/environment.js
+++ config/environment.js
@@ -20,6 +20,18 @@ module.exports = function(environment) {
     APP: {
       // Here you can pass flags/options to your application instance
       // when it is created
+    },
+    moment: {
+      // Options:
+      // 'all' - all years, all timezones
+      // '2010-2020' - 2010-2020, all timezones
+      // 'none' - no data, just timezone API
+      includeTimezone: 'all',
+      // Global Default Output Format
+      // Your application may require a default format other than the default, ISO 8601. 
+      // For example, you may want dates to fallback on the localized shorthand format L by default.
+    outputFormat: 'L', //Working
+      allowEmpty: true // default: false//Working : It will not show 'Invalid date' for empty date.
     }
   };
 
