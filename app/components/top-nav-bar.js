b18bc29ffb0cc0c9d4fd3a957b6b758b52901f6f
Vishu <vishu@getperx.com>
2016-12-19 14:24:29 +0800

    Testing new component of dashboards.

--- app/components/top-nav-bar.js
+++ app/components/top-nav-bar.js
@@ -3,6 +3,25 @@ import Ember from 'ember';
 export default Ember.Component.extend({
   tagName: '',
   session: Ember.inject.service(),
+  messages :
+  [
+  { 
+    id: 1,
+    userName:"Vishu",
+    userEmail: 'vishusharmagit@gmail.com',
+    messageContent: "Message From Server 1",
+    messageTime: new Date(),
+    profileImageUrl:"http://vignette2.wikia.nocookie.net/althistory/images/a/a6/Venstre_(Denmark)_Logo.png/revision/latest?cb=20081224190313" 
+  },
+  { 
+    id: 1,
+    userName:"Vishu",
+    userEmail: 'vishusharmagit@gmail.com',
+    messageContent: "Message From Server 2",
+    messageTime: new Date(),
+    profileImageUrl:"http://vignette2.wikia.nocookie.net/althistory/images/a/a6/Venstre_(Denmark)_Logo.png/revision/latest?cb=20081224190313" 
+  }
+  ],
   actions : {
 		invalidateSession() {
       this.get('session').invalidate();
